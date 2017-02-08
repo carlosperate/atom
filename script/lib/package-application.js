e90d7d147d9b9e90afcb14229c6bbf0756c9e095
carlosperate <carlosperate@users.noreply.github.com>
2017-02-08 16:48:58 +0000

    Update electron-packager to v8 and force arm7l arch

--- script/lib/package-application.js
+++ script/lib/package-application.js
@@ -18,12 +18,12 @@ module.exports = function () {
     'app-bundle-id': 'com.github.atom',
     'app-copyright': `Copyright © 2014-${(new Date()).getFullYear()} GitHub, Inc. All rights reserved.`,
     'app-version': CONFIG.appMetadata.version,
-    'arch': (() => {
-      if (process.platform === 'linux') {
-        return process.arch
-      } else {
-        return process.platform === 'win32' ? 'ia32' : 'x64'
-      }})(),
+    'arch': 'armv7l', //(() => {
+      //if (process.platform === 'linux') {
+      //  return process.arch
+      //} else {
+      //  return process.platform === 'win32' ? 'ia32' : 'x64'
+      //}})(),
     'asar': {unpack: buildAsarUnpackGlobExpression()},
     'build-version': CONFIG.appMetadata.version,
     'download': {cache: CONFIG.electronDownloadPath},
